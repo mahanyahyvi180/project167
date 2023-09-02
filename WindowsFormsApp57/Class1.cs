c0bb413fe76f2e5c39da496802fc707cc2b0da72
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-09-02 21:07:24 +0330

    Add files via upload

---
+++ WindowsFormsApp57/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp57
+{
+ public  class MyMath
+    {
+        public int x;
+        public int y;
+
+        public int sum()
+        {
+            return x + y;
+        }
+
+        public static int newsum(int x, int y)
+        {
+            return x + y;
+        }
+      
+        
+      
+     
+        
+
+       
+    }
+}
